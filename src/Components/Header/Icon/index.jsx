20a619f81191bf0985ed6474e942c626650bb598
Cri7Ro <kri7ro@mail.ru>
2021-08-13 17:04:36 +0300

    header ready

--- src/Components/Header/Icon/index.jsx
+++ src/Components/Header/Icon/index.jsx
@@ -1,9 +1,10 @@
 import react from 'react';
+import iconStyles from './Icon.module.scss'; 
 
 const Icon = ({imgSrc, text}) => {
     return (
-        <div>
-            <img src={imgSrc} alt="" />
+        <div className={iconStyles.icon}>
+            <img src={imgSrc} alt="" height='14px'/>
             <p>{text}</p>
         </div>
     );
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -10,20 +10,22 @@ const Header = () => {
                <Menu />
                 <p>+7 (921) 912-34-56</p>   
             </section>
-            <section>
-                <img src="./image/header/logo.png" alt="" />
-                <div>
-                    <input type="text" placeholder="Поиск по каталогу"/>
+            <section className={headerStyles.header_main_content}>
+                <img src="./image/header/logo.png" alt="" height='48px'/>
+                <div className={headerStyles.search}>
+                    <input type="text" placeholder="Поиск по каталогу" className={headerStyles.search_input}/>
                     <button><img src="./image/header/search.png" alt="" /></button>
                 </div>
-                <div>
-                    <Icon imgSrc={'./image/header/stats.png'} text='Сравнение'/>
-                    <Icon imgSrc={'./image/header/like.png'} text='Избранное'/>
-                    <Icon imgSrc={'./image/header/user.png'} text='Войти'/>
-                </div>
-                <div>
-                    <img src="./image/header/basket.png" alt="" />
-                    <p>6 500 P</p>
+                <div className={headerStyles.user_info}>
+                    <div className={headerStyles.icon_container}>
+                        <Icon imgSrc={'./image/header/stats.png'} text='Сравнение'/>
+                        <Icon imgSrc={'./image/header/like.png'} text='Избранное'/>
+                        <Icon imgSrc={'./image/header/user.png'} text='Войти'/>
+                    </div>
+                    <div className={headerStyles.basket}>
+                        <img src="./image/header/basket.png" alt="" />
+                        <p>6 500 P</p>
+                    </div>
                 </div>
             </section>
         </header>
