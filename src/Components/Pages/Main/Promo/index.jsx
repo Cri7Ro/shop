6f44238047cf6035c364c1a938b968f6eaeabf1f
Cri7Ro <kri7ro@mail.ru>
2021-08-16 11:56:25 +0300

    promo changed

--- src/Components/Pages/Main/Promo/index.jsx
+++ src/Components/Pages/Main/Promo/index.jsx
@@ -1,25 +1,65 @@
-import react from 'react';
+import react, { useEffect, useRef, useState } from 'react';
+import promoData from '../../../../Data/promoData';
 import promoStyles from './Promo.module.scss';
 
 
 const Promo = () => {
+
+    const [currentPromo, setCurrentPromo] = useState(0);
+    const [lineArray, setLineArray] = useState([]);
+    const line1Ref = useRef(null);
+    const line2Ref = useRef(null);
+    const line3Ref = useRef(null);
+    const line4Ref = useRef(null);
+
+    useEffect(() => {
+        if (line1Ref) line1Ref.current.classList.add(promoStyles.active);
+    }, [line1Ref]);
+
+    useEffect(() => {
+        if (line1Ref && line2Ref && line3Ref && line4Ref) setLineArray([...lineArray, line1Ref, line2Ref, line3Ref, line4Ref]);
+    }, [line1Ref, line2Ref,line3Ref, line4Ref]);
+
+    useEffect (() => {
+        if (lineArray[currentPromo]) {
+            lineArray[currentPromo].current.classList.add(promoStyles.active);
+        }
+    }, [currentPromo]);
+
+    function handleLeftClick() {
+        lineArray[currentPromo].current.classList.remove(promoStyles.active);
+        if (promoData[currentPromo- 1]) {
+            setCurrentPromo(prev => prev - 1);
+        } else {
+            setCurrentPromo(promoData.length - 1);
+        }
+    }
+
+    function handleRightClick() {
+        lineArray[currentPromo].current.classList.remove(promoStyles.active);
+        if (promoData[currentPromo + 1]) {
+            setCurrentPromo(prev => prev + 1);
+        } else {
+            setCurrentPromo(0);
+        }
+    }
+
     return (
         <section className={promoStyles.promo_container}>
             <div className={promoStyles.promo}>
                 <div className={promoStyles.promoText_container}>
-                    <button id="left"><img src="./image/main_page/left.png" alt="" /></button>
+                    <button id="left" onClick={handleLeftClick}><img src="./image/main_page/left.png" alt="" /></button>
                     <div className={promoStyles.promoText}>
-                        <h2>Акция лета 2021 – на все чехлы iphone</h2>
-                        <h1>1+1=3</h1>
+                        <h1>{promoData[currentPromo].promo}</h1>
                     </div>
                 </div>
-                <button id="right"><img src="./image/main_page/right.png" alt="" /></button>
+                <button id="right" onClick={handleRightClick}><img src="./image/main_page/right.png" alt="" /></button>
             </div>
             <div className={promoStyles.promo_line_container}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div ref={line1Ref}></div>
+                <div ref={line2Ref}></div>
+                <div ref={line3Ref}></div>
+                <div ref={line4Ref}></div>
             </div>
         </section>
     );
